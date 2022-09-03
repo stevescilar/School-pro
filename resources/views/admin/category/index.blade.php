ef2d3e0857780afe125763dcf1e5d5e1c7fd9f64
stevescilar <stevemuambi@gmail.com>
2022-09-03 22:42:31 +0300

    update

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -14,7 +14,7 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="card">
-
+                    <!-- human interraction installed -->
                     @if(session('success'))
                     <div class="alert alert-success alert-dismissible fade show" role="alert">
                         <strong>{{ session('success')}}</strong> 
