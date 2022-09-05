f359932f4125a0f54f801d6590666610b7dde639
stevescilar <stevemuambi@gmail.com>
2022-09-05 16:29:25 +0300

    added ORM

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use Auth;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -24,17 +25,25 @@ class CategoryController extends Controller
         ]); 
 
         // Insert data using Eloquent ORM 
-        Category::insert([
-            'category_name' => $request->category_name,
-            'user_id' => Auth::user()->id,
-            'created_at' => Carbon::now()
+        // Category::insert([
+        //     'category_name' => $request->category_name,
+        //     'user_id' => Auth::user()->id,
+        //     'created_at' => Carbon::now()
 
-        ]);
+        // ]);
 
-        // $category = new Category;
-        // $category -> category_name = $request->category_name;
-        // $category -> user_id = Auth::user()->id;
-        // $category->save();
+            // Query builder
+
+        // $data = array();
+        // $data['category_name'] = $request->category_name;
+        // $data['user_id'] = Auth::user()->id;
+        // DB::table('categories')->insert($data);
+
+
+        $category = new Category;
+        $category -> category_name = $request->category_name;
+        $category -> user_id = Auth::user()->id;
+        $category->save();
 
         return Redirect()->back()->with('success','Category Added Successfully');
 
