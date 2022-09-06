d70f9675b169492a20f8e2feba1fa1ea424272e1
stevescilar <stevemuambi@gmail.com>
2022-09-06 15:21:20 +0300

    upload image/edit/update

--- resources/views/admin/brand/edit.blade.php
+++ resources/views/admin/brand/edit.blade.php
@@ -16,12 +16,20 @@
 
         <div class="col-md-8">
             <div class="card">
+                             <!-- hci-->
+                     @if(session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success')}}</strong> 
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
                 <div class="card-header">
                     Edit Brand
                 </div>
                 <div class="card-body">
-                <form action="{{ url('brand/update/'.$brands->id) }}" method="POST">
+                <form action="{{ url('brand/update/'.$brands->id) }}" method="POST" enctype="multipart/form-data">
                     @csrf
+                    <input type="hidden" name="old_image" value="{{ $brands->brand_image }}">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Update Brand Name</label>
                         <input type="text" name="brand_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $brands->brand_name }}">
