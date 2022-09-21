420106d29cf7e0da8e36373982574abdd641a665
stevescilar <stevemuambi@gmail.com>
2022-09-21 06:50:24 +0300

    contact update

--- database/migrations/2022_09_20_061023_create_contacts_table.php
+++ database/migrations/2022_09_20_061023_create_contacts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->text('name');
             $table->text('address');
             $table->string('email');
             $table->string('phone');
